c8b02fd4b99ea08aba25cea7ccf0eee3b6d1ab0b
junho2408 <bagho1999@gmail.com>
2025-02-04 15:44:45 +0900

    fix/깃허브 로그인 시 콜백 API가 2번 호출되는 에러 수정

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import useUserStore from "@/stores/userStore";
 
@@ -5,30 +6,41 @@ const useAuth = () => {
   const navigate = useNavigate();
   const { setUser } = useUserStore();
 
-  const handleLogin = async (code: string) => {
-    try {
-      const response = await fetch(`http://localhost:5000/auth/github/callback?code=${code}`);
+  const handleLogin = useCallback(
+    async (code: string) => {
+      try {
+        const response = await fetch(
+          `http://localhost:5000/auth/github/callback?code=${code}`
+        );
 
-      if (response.ok) {
-        const data = await response.json();
+        if (response.ok) {
+          const data = await response.json();
 
-        localStorage.setItem("accessToken", data.token.accessToken);
-        localStorage.setItem("userInfo", JSON.stringify(data.user));
+          localStorage.setItem("accessToken", data.token.accessToken);
+          localStorage.setItem("userInfo", JSON.stringify(data.user));
 
-        const userInfo = JSON.parse(localStorage.getItem("userInfo") || "{}")
+          const userInfo = JSON.parse(localStorage.getItem("userInfo") || "{}");
 
-        setUser(userInfo.username, userInfo.id, userInfo.avatar_url, userInfo.bio, "SIGNED_IN")
+          setUser(
+            userInfo.username,
+            userInfo.id,
+            userInfo.avatar_url,
+            userInfo.bio,
+            "SIGNED_IN"
+          );
 
-        navigate("/boards/study/posts");
-      } else {
-        console.error("Login callback failed:", await response.text());
-        alert("로그인 실패");
+          navigate("/boards/study/posts");
+        } else {
+          console.error("Login callback failed:", await response.text());
+          alert("로그인 실패");
+        }
+      } catch (error) {
+        console.error("Error during login callback:", error);
+        alert("로그인 중 에러가 발생했습니다.");
       }
-    } catch (error) {
-      console.error("Error during login callback:", error);
-      alert("로그인 중 에러가 발생했습니다.");
-    }
-  };
+    },
+    [navigate, setUser]
+  );
 
   return { handleLogin };
 };
